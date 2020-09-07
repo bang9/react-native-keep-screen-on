7df555eea3d38fdffc942f64b9e7c73aa9490e8c
bang9dev <gusrn1423@naver.com>
2020-09-07 10:05:01 +0900

    feat: added hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
+import React from 'react'
 import { NativeModules } from 'react-native'
 
-const RNKeepScreenOn = NativeModules['RNKeepScreenOn'];
-if(!RNKeepScreenOn) throw new Error('Not found module');
+const RNKeepScreenOn = NativeModules.RNKeepScreenOn
+if (!RNKeepScreenOn) throw new Error('Not found module')
 
 type KeepScreenOnType = {
-    on: () => void;
-    off: () => void;
+  on: () => void
+  off: () => void
+  useKeepScreenOn: () => void
 }
 
 const KeepScreenOn: KeepScreenOnType = {
-    on: RNKeepScreenOn.on,
-    off: RNKeepScreenOn.off
+  on: RNKeepScreenOn.on,
+  off: RNKeepScreenOn.off,
+  useKeepScreenOn: () => {
+    React.useEffect(() => {
+      RNKeepScreenOn.on()
+      return () => RNKeepScreenOn.off()
+    }, [])
+  },
 }
 
-export default KeepScreenOn;
+export default KeepScreenOn
